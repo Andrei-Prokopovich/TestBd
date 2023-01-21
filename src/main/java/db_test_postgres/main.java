278031f387ec5f5596fa2a1bcdea964049ea1a34
Andrei-Prokopovich <120657073+Andrei-Prokopovich@users.noreply.github.com>
2023-01-21 13:41:40 +0300

    test1

--- src/main/java/db_test_postgres/main.java
+++ src/main/java/db_test_postgres/main.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class main {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USERNAME = "root";
+    private static final String USERNAME = "postgres";
     private static final String PASSWORD = "joker43PRO32";
 
 
