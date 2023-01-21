cd4b94ee98dd44f6e6418401bd762984acaca01c
Andrei-Prokopovich <120657073+Andrei-Prokopovich@users.noreply.github.com>
2023-01-21 13:28:54 +0300

    test1

---
+++ src/main/java/db_test_postgres/main.java
@@ -0,0 +1,36 @@
+package db_test_postgres;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class main {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/posrgres";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "joker43PRO32";
+
+
+    public static void main(String[] argh){
+        Connection connection;
+
+        try{
+            Driver driver = new org.postgresql.Driver();
+            DriverManager.registerDriver(driver);
+
+
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            if(!connection.isClosed()){
+                System.out.println("Connection BD alright!");
+            }
+
+            connection.close();
+            if(connection.isClosed()) {
+                System.out.println("Connection BD closed!");
+            }
+        } catch (SQLException e){
+            System.err.println("Not download class driver");
+        }
+    }
+}
